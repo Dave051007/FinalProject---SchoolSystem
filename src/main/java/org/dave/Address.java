9d6f6e233e72b38dc0274dad4fe9f2b624e5fdff
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-05 17:23:53 -0500

    added an AllArgumentConstructor: if postal code valid set all fields, else set to default

--- src/main/java/org/dave/Address.java
+++ src/main/java/org/dave/Address.java
@@ -7,6 +7,22 @@ public class Address {
     private Province province;
     private String postalCode;
 
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode;
+        } else  {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
+
     /**
      * Checks if the postal code is valid
      * @param postalCode the postal code to be checked
