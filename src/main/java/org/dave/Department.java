2884d71f59f08bb0ff807810cb75cd065f79deb7
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-06 12:13:06 -0500

    Modified constructor to check if departmentName is valid or not

--- src/main/java/org/dave/Department.java
+++ src/main/java/org/dave/Department.java
@@ -13,8 +13,13 @@ public class Department {
     private static int nextId = 0;
 
     public Department(String departmentName) {
-        this.departmentName = departmentName;
-        this.departmentId = String.format("%05d", nextId++);
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = String.format("%05d", nextId++);
+        } else {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
     }
 
     /**
