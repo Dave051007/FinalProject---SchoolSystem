609c2c9bc01e656f755bf6719f037191db6809f1
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-06 12:33:35 -0500

    Fixed spacing in the Department class fields

--- src/main/java/org/dave/Department.java
+++ src/main/java/org/dave/Department.java
@@ -10,6 +10,7 @@ import lombok.ToString;
 public class Department {
     private String departmentName;
     private String departmentId;
+
     private static int nextId = 1;
 
     public Department(String departmentName) {
