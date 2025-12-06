e30afef35bd332b30378ecea577145c605fcc004
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-06 12:29:42 -0500

    Added a Student class with its fields

---
+++ src/main/java/org/dave/Student.java
@@ -0,0 +1,13 @@
+package org.dave;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private List<Course> registerdCourses;
+
+    private static int nextId = 1;
+
+}
