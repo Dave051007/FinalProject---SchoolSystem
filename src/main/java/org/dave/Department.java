52645472fb5c97f2ed08dd0604ec4bcd70680188
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-06 11:52:50 -0500

    Added a constructor

--- src/main/java/org/dave/Department.java
+++ src/main/java/org/dave/Department.java
@@ -1,4 +1,12 @@
 package org.dave;
 
 public class Department {
+    private String departmentName;
+    private String departmentId;
+    private static int nextId = 0;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+        this.departmentId = String.format("%05d", nextId++);
+    }
 }
