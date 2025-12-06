b47ae6ae8d184c0d6670a4347b9543e7a6e898e9
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-06 12:08:16 -0500

    Added a method which checks if the departmentName is valid or invalid

--- src/main/java/org/dave/Department.java
+++ src/main/java/org/dave/Department.java
@@ -16,4 +16,27 @@ public class Department {
         this.departmentName = departmentName;
         this.departmentId = String.format("%05d", nextId++);
     }
+
+    /**
+     * Checks if the department name is valid or invalid
+     * @param departmentName the given department name
+     * @return true if department name is valid, false if department name is invalid
+     */
+    public boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null) {
+            return false;
+        }
+
+        if (departmentName.isBlank()) {
+            return false;
+        }
+
+        for (char  c : departmentName.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
