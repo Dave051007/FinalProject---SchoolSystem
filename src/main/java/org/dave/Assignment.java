f1767ff393fd10ac473108a4fad1cfc3b3ffb11a
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-07 09:34:56 -0500

    Added fields to the Assignment class

--- src/main/java/org/dave/Assignment.java
+++ src/main/java/org/dave/Assignment.java
@@ -1,4 +1,14 @@
 package org.dave;
 
+import java.util.List;
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private List<Integer> courses;
+
+    private static int nextId = 0;
+
+
 }
