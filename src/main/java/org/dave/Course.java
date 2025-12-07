3e36235b81e2effe34a774bd538accf883ede2cd
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-07 10:38:46 -0500

    Added fields to the course class

--- src/main/java/org/dave/Course.java
+++ src/main/java/org/dave/Course.java
@@ -1,4 +1,14 @@
 package org.dave;
 
+import java.util.List;
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private List<Student> registeredStudents;
+    private List<Assignment> assignments;
+
+    private static int nextId = 1;
 }
