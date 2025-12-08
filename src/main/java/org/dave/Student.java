6e64a98aaf4613bc6847e7a23ce56ce468f46349
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-08 08:54:22 -0500

    Added a constructor for the Student class

--- src/main/java/org/dave/Student.java
+++ src/main/java/org/dave/Student.java
@@ -1,5 +1,8 @@
 package org.dave;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String studentId;
     private String studentName;
@@ -9,6 +12,15 @@ public class Student {
     private List<Course> registerdCourses;
 
     private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registerdCourses = new ArrayList<>();
+    }
 
     public enum Gender {
         MALE, FEMALE
