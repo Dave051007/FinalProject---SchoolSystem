ce76d7fc69134bac231c0bf464a6f5fcf21fc866
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2025-12-14 19:01:41 -0500

    Added toTitleCase method

--- src/main/java/org/dave/Util.java
+++ src/main/java/org/dave/Util.java
@@ -1,4 +1,25 @@
 package org.dave;
 
 public class Util {
+
+    /**
+     * Converts each word in a string to title case --> Xxxxxx
+     * @param str the string to be converted to title case
+     * @return the string in title case
+     */
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        String strInTitleCase = "";
+        String[] strArray = str.split(" ");
+        for (String word : strArray) {
+            strInTitleCase += word.substring(0, 1).toUpperCase() +
+                              word.substring(1).toLowerCase() +
+                              " ";
+        }
+
+        return strInTitleCase.trim();
+    }
 }
